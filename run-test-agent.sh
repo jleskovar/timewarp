537af191c443348354ef789b1ff0fd8f07a52359
James Leskovar <jleskovar@gmail.com>
2016-04-17 18:42:05 +1000

    adding support to startup test program with a JMX listener on port 9010

--- run-test-agent.sh
+++ run-test-agent.sh
@@ -7,6 +7,11 @@ set -e
 TIMEWARP_JAR=$(ls build/libs/timewarp-*.jar | grep -v javadoc | grep -v sources)
 java -javaagent:$TIMEWARP_JAR \
 -Xbootclasspath/a:$TIMEWARP_JAR \
+-Dcom.sun.management.jmxremote \
+-Dcom.sun.management.jmxremote.port=9010 \
+-Dcom.sun.management.jmxremote.local.only=false \
+-Dcom.sun.management.jmxremote.authenticate=false \
+-Dcom.sun.management.jmxremote.ssl=false \
 -cp build/classes/main:build/classes/test \
 co.paralleluniverse.test.TimeTester &
 
